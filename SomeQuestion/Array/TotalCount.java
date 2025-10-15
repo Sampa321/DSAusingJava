5b8dee07b555762793499b563ed2782752fe96c1
Sampa Nayak <sampanayak987@gmail.com>
2025-10-15 23:56:22 +0530

    Add files via upload

---
+++ SomeQuestion/Array/TotalCount.java
@@ -0,0 +1,50 @@
+package HomePractice.GeeksForGeeks.Arrays;
+
+import java.util.Scanner;
+
+/*
+You are given an array arr[] of positive integers and a threshold value k. For each element in the array, divide it into the minimum number of small integers such that each divided integer is less than or equal to k. Compute the total number of these integer across all elements of the array.
+
+Examples:
+
+Input: k = 3, arr[] = [5, 8, 10, 13]
+Output: 14
+Explanation: Each number can be expressed as sum of different numbers less than or equal to k as 5 (3 + 2), 8 (3 + 3 + 2), 10 (3 + 3 + 3 + 1), 13 (3 + 3 + 3 + 3 + 1). So, the sum of count of each element is (2+3+4+5)=14.
+Input: k = 4, arr[] = [10, 2, 3, 4, 7]
+Output: 8
+Explanation: Each number can be expressed as sum of different numbers less than or equal to k as 10 (4 + 4 + 2), 2 (2), 3 (3), 4 (4) and 7 (4 + 3).So, the sum of count of each element is (3 + 1 + 1 + 1 + 2) = 8.
+ */
+public class TotalCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array size:");
+        int n = sc.nextInt();
+        System.out.println("Enter the array elements:");
+        int []arr = new int[n];
+        for(int i=0;i< arr.length;i++)
+        {
+            System.out.print("arr["+i+"]=");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("enter the value of k : ");
+        int k=sc.nextInt();
+        System.out.println("Total count : "+totalCount(k,arr));
+    }
+   static int totalCount(int k, int[] arr) {
+        // code here
+        int sum=0;
+        for(int ele:arr)
+        {
+            int r=ele/k;
+            if(ele%k==0)
+            {
+                sum+=r;
+            }
+            else
+            {
+                sum+=(r+1);
+            }
+        }
+        return sum;
+    }
+}
