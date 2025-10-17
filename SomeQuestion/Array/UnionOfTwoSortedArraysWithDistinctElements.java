8b0f273ea4fb5361187f723069e1a85346dbcd3b
Sampa Nayak <sampanayak987@gmail.com>
2025-10-18 00:22:30 +0530

    Add files via upload

---
+++ SomeQuestion/Array/UnionOfTwoSortedArraysWithDistinctElements.java
@@ -0,0 +1,66 @@
+package HomePractice.GeeksForGeeks.Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/*
+Given two sorted arrays a[] and b[], where each array contains distinct elements , the task is to return the elements in the union of the two arrays in sorted order.
+
+Union of two arrays can be defined as the set containing distinct common elements that are present in either of the arrays.
+Examples:
+
+Input: a[] = [1, 2, 3, 4, 5], b[] = [1, 2, 3, 6, 7]
+Output: 1 2 3 4 5 6 7
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5 6 7.
+Input: a[] = [2, 3, 4, 5], b[] = [1, 2, 3, 4]
+Output: 1 2 3 4 5
+Explanation: Distinct elements including both the arrays are: 1 2 3 4 5.
+Input: a[] = [1], b[] = [2]
+Output: 1 2
+Explanation: Distinct elements including both the arrays are: 1 2.
+ */
+public class UnionOfTwoSortedArraysWithDistinctElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the 1st array size:");
+        int n1= sc.nextInt();
+        int []arr1 = new int[n1];
+        System.out.println("Enter the 1st array elements:");
+        for(int i=0;i<n1;i++)
+        {
+            System.out.print("arr1["+i+"]=");
+            arr1[i] = sc.nextInt();
+        }
+        System.out.print("Enter the 2nd array size:");
+        int n2= sc.nextInt();
+        int []arr2 = new int[n2];
+        System.out.println("Enter the 2nd array elements:");
+        for(int i=0;i<n2;i++)
+        {
+            System.out.print("arr2["+i+"]=");
+            arr2[i] = sc.nextInt();
+        }
+        System.out.println("After union , ArrayList is : "+findUnion(arr1,arr2));
+    }
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // add your code here
+        HashSet<Integer> set =new HashSet<>();
+        for(int ele:a)
+        {
+            set.add(ele);
+        }
+        for(int ele:b)
+        {
+            set.add(ele);
+        }
+        ArrayList<Integer> list =new ArrayList<Integer>();
+        for(int ele:set)
+        {
+            list.add(ele);
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
