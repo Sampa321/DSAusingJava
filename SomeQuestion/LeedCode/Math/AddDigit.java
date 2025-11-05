30f428cd3502938d49a446b396096a042bc75c59
Sampa Nayak <sampanayak987@gmail.com>
2025-11-05 22:44:56 +0530

    Create AddDigit.java for digit addition
    Implement a program to add digits of a number until one digit remains.

---
+++ SomeQuestion/LeedCode/Math/AddDigit.java
@@ -0,0 +1,39 @@
+package HomePractice.LeedCode.Math.Easy;
+
+import java.util.Scanner;
+
+/* 258.
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ */
+public class AddNumber {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n=sc.nextInt();
+        System.out.println("Add digit is : "+addDigits(n));
+    }
+    public static int addDigits(int num) {
+        if(num==0)
+        {
+            return 0;
+        }
+        else if(num%9==0)
+        {
+            return 9;
+        }
+        return num%9;
+    }
+}
