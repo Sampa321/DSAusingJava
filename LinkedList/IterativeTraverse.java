0bb811d4e2353b9c73e185788f80d3ae6522cd69
Sampa Nayak <sampanayak987@gmail.com>
2025-11-13 22:04:52 +0530

    Add files via upload

---
+++ LinkedList/IterativeTraverse.java
@@ -0,0 +1,37 @@
+package ClassPractice.MainTopic.LinkedList.Traversal;
+
+public class IterativeTraverse {
+    int data;
+    IterativeTraverse next;
+    IterativeTraverse(int value)
+    {
+        this.data = value;
+        this.next = null;
+    }
+    public static void main(String[] args) {
+        /*IterativeTraverse head = new IterativeTraverse(10);
+        IterativeTraverse p1 = new IterativeTraverse(20);
+        IterativeTraverse p2= new IterativeTraverse(30);
+        IterativeTraverse p3 = new IterativeTraverse(40);
+        head.next = p1;
+        p1.next = p2;
+        p2.next = p3;*/
+        //IterativeTraverse head = new IterativeTraverse(10);
+        IterativeTraverse head = null;
+        traverse(head);
+    }
+    public static void traverse(IterativeTraverse head)
+    {
+        if(head ==null)
+        {
+            System.out.println("Bhai list empty!");
+            return;
+        }
+        IterativeTraverse current = head;
+        while (current!=null)
+        {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
