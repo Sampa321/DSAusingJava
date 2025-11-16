6d9fa85aaaf43952c8b6a25491a876e654ff8b51
Sampa Nayak <sampanayak987@gmail.com>
2025-11-16 23:27:33 +0530

    Create DeleteFirstNode.java

---
+++ LinkedList/DeleteFirstNode.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Node {
+    int data;
+    Node next;
+    public Node(int value)
+    {
+        this.data = value;
+        this.next = null;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node head = new Node(23);
+        Node p1 = new Node(787);
+        Node p2 = new Node(56);
+        Node p3 = new Node(66);
+        head.next = p1;
+        p1.next = p2;
+        p2.next = p3;
+        head = deleteFirstNode(head);
+        Node current = head;
+        while(current!= null){
+            System.out.print(current.data+" ");
+            current = current.next;
+        }
+    }
+    public static Node deleteFirstNode(Node head){
+        if(head == null){
+            return head;
+        }
+        return head.next;
+    }
+}
