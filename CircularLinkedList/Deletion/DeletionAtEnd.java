84bcff4fd156ec30eb035dc49a099398aa8df47f
Sampa Nayak <sampanayak987@gmail.com>
2025-12-19 20:29:13 +0530

    Add files via upload

---
+++ CircularLinkedList/Deletion/DeletionAtEnd.java
@@ -0,0 +1,38 @@
+package ClassPractice.MainTopic.CircularLinkedList.Deletion;
+
+import ClassPractice.MainTopic.CircularLinkedList.Node;
+
+public class DeletionAtEnd {
+
+    static void main() {
+        //Node head = null;
+        Node head = new Node(12);
+        head.next= head;
+        /*Node head = new Node(23);
+        Node p1 = new Node(78);
+        Node p2 = new Node(56);
+        Node p3 = new Node(156);
+        head.next = p1;
+        p1.next = p2;
+        p2.next = p3;
+        p3.next = head;*/
+        System.out.print("Before delete, cll is :");
+        Node.traverse(head);
+        head = delete(head);
+        System.out.println();
+        System.out.print("After delete, cll is :");
+        Node.traverse(head);
+
+    }
+    public static Node delete(Node head) {
+        if (head == null || head.next == head) {
+            return null;
+        }
+        Node curr = head;
+        while (curr.next.next != head) {
+            curr = curr.next;
+        }
+        curr.next = head;
+        return head;
+    }
+}
