f6868fc451797788d2ce5a29b9b1aa1734983dfc
Sampa Nayak <sampanayak987@gmail.com>
2025-12-27 21:57:50 +0530

    Add files via upload

---
+++ DoubleLinkedList/Delete/DeletionAtBeginning.java
@@ -0,0 +1,34 @@
+package ClassPractice.MainTopic.DoublyLinkedList.Deletion;
+
+import ClassPractice.MainTopic.DoublyLinkedList.Node;
+
+public class DeletionAtBeginning {
+   public static void main() {
+        //Node head = null;
+        //Node head = new Node(22);
+        Node head = new Node(10);
+        Node p1 = new Node(20);
+        Node p2 = new Node(30);
+        Node p3 = new Node(40);
+        head.next = p1;
+        p1.prev = head;
+        p1.next = p2;
+        p2.prev = p1;
+        p2.next = p3;
+        p3.prev = p2;
+        System.out.print("Before delete, Dll is :");
+        Node.traverse(head);
+        System.out.println();
+        head = delete(head);
+        System.out.print("After delete, Dll is :");
+        Node.traverse(head);
+    }
+    public static Node delete(Node head){
+        if(head == null || head.next == null)
+        {
+            return null;
+        }
+        head.next.prev = null;
+        return head.next;
+    }
+}
