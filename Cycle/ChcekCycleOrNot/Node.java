240bb8f46b34c755810bf6df240251a1373fcdb7
Sampa Nayak <sampanayak987@gmail.com>
2025-12-28 18:57:32 +0530

    Add files via upload

---
+++ Cycle/ChcekCycleOrNot/Node.java
@@ -0,0 +1,12 @@
+package ClassPractice.MainTopic.Cycle;
+
+public class Node {
+    int data;
+    Node next;
+    Boolean visited;
+    public Node(int value)
+    {
+        this.data = value;
+        this.visited = false;
+    }
+}
